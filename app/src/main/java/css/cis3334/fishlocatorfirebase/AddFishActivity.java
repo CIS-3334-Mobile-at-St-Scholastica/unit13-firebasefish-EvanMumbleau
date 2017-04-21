6750aef47ba6993f26430758d045d4ef48052661
emumbleau <emumbleau@css.edu>
2017-04-21 11:22:16 -0500

    In Class

--- app/src/main/java/css/cis3334/fishlocatorfirebase/AddFishActivity.java
+++ app/src/main/java/css/cis3334/fishlocatorfirebase/AddFishActivity.java
@@ -1,8 +1,12 @@
 package css.cis3334.fishlocatorfirebase;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -32,11 +36,21 @@ public class AddFishActivity extends AppCompatActivity {
         fishDataSource.open();
 
         // get the current location of the phone
-//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        Criteria criteria = new Criteria();
-//        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-//        lattitude = location.getLatitude();
-//        longiture = location.getLongitude();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        //lattitude = location.getLatitude();
+        //longiture = location.getLongitude();
 
         // set up the button listener
         buttonSave = (Button) findViewById(R.id.buttonSave);
